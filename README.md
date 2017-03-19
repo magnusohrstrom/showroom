7c283bea4fd21db6678f7cf08e45d31023ab6812
magnusohrstrom <magnusohrstrom330@gmail.com>
2017-03-19 19:25:48 +0100

    css start added hero

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # showroom
+## Ett virtuellt konstgalleri med Holländska Rijksmuseums samlingar.
+
+##Funktionalitet:
+Användaren ska kunna söka i bilddatabasen på konstnär, årtal, medium etc.
+
+Den ska även kunna göra sin egen skräddarsydda "visning där ett visst urval av blandade verk görs".
--- script/main.js
+++ script/main.js
@@ -1,21 +1,142 @@
-var jsonex = (function(){
+
+const showRoom = (function(){
+  let tempList = [];
+  let originList = [];
+
   return {
-    get: () => {
+    //Get
+
+    appendResultsToInterface: (list) => {
+
+      let section = document.getElementsByClassName('list-section')[0];
+      let movieHtml = '';
+
+      for(var i = 0 ; i < list.length; i++){
+        if(list[i].hasImage !== false){
+          movieHtml +=
+            `<img src="${list[i].webImage.url}">`;
+          }
+          else{}
+        }
+
+
+
+        section.innerHTML = movieHtml;
+
+    },
+    getFromSearchTitle: () => {
+      let query = document.getElementsByClassName('search-input')[0].value;
       $.ajax({
         method: 'GET',
-        dataType:'json',
-        //url:"http://www.rijksmuseum.nl/api/oai2/[mvBTcUjC]/",
-        url:"http://www.rijksmuseum.nl/api/oai2/mvBTcUjC/?verb=ListRecords&set=collectie_online&metadataPrefix=oai_dc",
+        dataType:'jsonp',
+        //beforeSend:function(xhr){xhr.setRequestHeader('Access-Control-Allow-Origin', '*');},
 
+        //url:"http://www.rijksmuseum.nl/api/oai2/mvBTcUjC/",
+        //url:"http://www.rijksmuseum.nl/api/oai2/[mvBTcUjC]/?verb=ListRecords&set=collectie_online&metadataPrefix=oai_dc",
+        url:`https://www.rijksmuseum.nl/api/en/collection?q=${query}&key=mvBTcUjC&format=jsonp&ps=100`,
 
         success: (response) => {
 
-          console.log(response);
+          tempList = response.artObjects;
+          showRoom.appendResultsToInterface(tempList);
+
+          console.log(tempList);
         },
         error:() => { console.log("error");}
-    });
-  }
-};
+
+
+      });
+    },
+    logTempList: () =>{
+      console.log(tempList);
+    },
+    //funciton that sets eventlisteners on app initiation.
+    init: () => {
+      document.getElementsByClassName('search-input')[0].addEventListener('input',showRoom.getFromSearchTitle);
+      document.getElementsByClassName('search-button')[0].addEventListener('click',showRoom.logTempList);
+
+
+    }
+};//end showRoom.
 })();
 
-jsonex.get();
+showRoom.init();
+
+
+//showRoom.getFromSearchTitle();
+
+/*
+function get(){
+  var init = {
+    method: 'GET'
+  }
+  fetch('https://www.rijksmuseum.nl/api/en/collection/sk-c-5?key=mvBTcUjC&format=json', init
+).then(function(response){
+  return response;
+  }).then(function(json){
+     console.log(json);
+   });
+}
+//get();
+
+//get();
+function jsonGET(){
+  $.ajax({
+    method: 'GET',
+    url:'https://www.rijksmuseum.nl/api/en/collection/sk-c-5?key=mvBTcUjC&format=jsonp',
+    dataType: 'jsonp', //change the datatype to 'jsonp' works in most cases
+    success: (res) => {
+          return res.artObject.webImage.url;
+   },
+    error: () => { console.log("error");}
+  });
+  var frame = document.getElementsByClassName('main-figure')[0];
+  var text = document.createTextNode(jsonGET.success);
+  frame.appendChild(text);
+  //frame.style.background = 'url('+jsonGET.success+')';
+}
+//jsonGET();
+function getshort(){
+  $.get({
+    url:'https://www.rijksmuseum.nl/api/en/collection/sk-c-5?key=mvBTcUjC&format=jsonp',
+    dataType: 'jsonp', //change the datatype to 'jsonp' works in most cases
+    success: (res) => {
+          var img = res.artObject.webImage.url;
+          var frame = document.getElementsByClassName('main-figure')[0];
+          var text = document.createTextNode(jsonGET.success);
+          frame.appendChild(text);
+          frame.style.background = 'url('+img+')';
+   },
+    error: () => { console.log("error");}
+  });
+}
+//getshort();
+function getshort2(){
+  $.get({
+    url:'https://www.rijksmuseum.nl/api/en/collection/sk-c-5?key=mvBTcUjC&format=jsonp',
+    dataType: 'jsonp', //change the datatype to 'jsonp' works in most cases
+    success: (res) => {
+          var img = res.artObject.webImage.url;
+          var frame = document.getElementsByTagName('img')[0];
+          var text = document.createTextNode(jsonGET.success);
+          frame.appendChild(text);
+          frame.setAttribute('src',img);
+   },
+    error: () => { console.log("error");}
+  });
+}
+
+//getshort2();
+
+function getshort3(){
+  $.get({
+    url:'https://rijksmuseum.nl/api/en/collection/sk-c-5?key=mvBTcUjC&format=json',
+    dataType: 'json', //change the datatype to 'jsonp' works in most cases
+    success: (res) => {
+          console.log(res);
+   },
+    error: () => { console.log("error");}
+  });
+}
+//getshort3();
+*/
