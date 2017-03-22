360f50e1c20f49e4da1e5d65b74024f32a5fe620
magnusohrstrom <magnusohrstrom330@gmail.com>
2017-03-22 15:07:28 +0100

    onsdag 14.

--- script/main.js
+++ script/main.js
@@ -2,9 +2,50 @@
 const showRoom = (function(){
   let tempList = [];
   let fullList = [];
+  let test = [
+    {title:'hej'},
+    {title:'hej2'},
+    {title:'hej3'},
+    {title:'hej4'}
+  ];
 
+  const ArtWork = {
+    create: function (title, long, maker, url){
+      var newArtWork = Object.create(this);
+
+      newArtWork.title = title;
+      newArtWork.longTitle = long;
+      newArtWork.principalOrFirstMaker = maker;
+      newArtWork.url = url;
+      return newArtWork;
+    },
+    getImgSrc: function() {
+      console.log(this.url);
+    }
+
+  };
 
   return {
+    ArtWork: ArtWork,
+
+    createNewArtWorkFromResponseList: function(list) {
+          for (var i = 0; i < list.length; i++) {
+            var title = list[i].title;
+            var longTitle = list[i].longTitle;
+            var maker = list[i].principalOrFirstMaker;
+            var url = list[i].webImage.url;
+            var artWork = showRoom.ArtWork.create(title,longTitle,maker,url);
+
+            fullList.push(artWork);
+          }
+          console.log(fullList);
+          //console.log(tempList);
+
+    },
+
+    createListOfObjectsFromFactory: function(artwork){
+      fullList.push(artwork);
+    },
     //Get function for all get requests.
     getShort: (url) => {
       return $.ajax({
@@ -21,7 +62,7 @@ const showRoom = (function(){
     },
 
     //Main search function/////////////////////////////////////////
-    getFromSearchQuery: () => {
+    getFromSearchQuery: function()  {
 
       let query = document.getElementsByClassName('search-input')[0].value;
       let artist = document.getElementById('artist-select').value;
@@ -30,66 +71,57 @@ const showRoom = (function(){
       let yearTo = document.getElementById('year-to').value;
 
       showRoom.getShort(`https://www.rijksmuseum.nl/api/en/collection/?q=${query}&involvedMaker=${artist}&type=${type}&imgonly=True&toppieces=True&yearfrom=${yearFrom}&yearto=${yearTo}&ps=100&key=WU1Jjq7U&format=json&st=OBJECTS`)
-        .done(function(response){
+        .then(function(response){
           console.log(response);
           tempList = response.artObjects;
+
           showRoom.appendResponseToInterface(tempList);
+          console.log(tempList);
+          showRoom.createNewArtWorkFromResponseList(tempList);
+        }).done(function(){showRoom.appendImgToMainFigure(tempList);});
 
-
-        });
       },
 
 
 
     //Appends search-result-respond to html-interface.
-    appendResponseToInterface:(list) => {
+    appendResponseToInterface:function(list) {
       let Html = '';
       let section = document.getElementsByClassName('list-section')[0];
       for (let i = 0; i < list.length; i++) {
         if(list[i].webImage !== null){
-          Html += `<figure class="list-figure">
+          Html += `<figure class="list-figure" onClick="showRoom.appendImgToMainFigureOnClick(this);">
                           <img src="${list[i].webImage.url}">
-                          <h4>${list[i].title}</h4>
+                          <h4>${list[i].longTitle}</h4>
                           <p>${list[i].principalOrFirstMaker}</p>
                         </figure>`;
         }
       }
       section.innerHTML = Html;
-      /*
-let Html = '';
-      let section = document.getElementsByClassName('list-section')[0];
-      for (let i = 0; i < list.length; i++) {
-        if(list[i].webImage !== null){
-          Html += `<figure class="list-figure">
-                          <img src="${list[i].webImage.url}">
-                          <h3>${list[i].title}</h3>
-                          <p>${list[i].principalMaker}</p>
-                          <p>${list[i].dating.year}</p>
-                        </figure>`;
-          }
-        }
-      section.innerHTML = Html;*/
-
     },
 
-    appendHtmlForEachResponse: (object) => {
-      var section = document.getElementsByClassName('list-section')[0];
+    appendImgToMainFigure: (list) => {
+      let mainImg = document.getElementsByClassName('main-figure')[0];
 
-      if(object.webImage !== null){
-        Html += `<figure class="list-figure">
-                        <img src="${object.webImage.url}">
-                        <h3>${object.title}</h3>
-                        <p>${object.principalMaker}</p>
-                        <p>${object.dating.year}</p>
-                      </figure>`;
-        }
+      //console.log(list);
+      mainImg.innerHTML = `<img src="${list[0].webImage.url}">`;
+    },
 
-    section.innerHTML = Html;
+    appendImgToMainFigureOnClick: function(img) {
+
+    var name = img.src;
+    alert(name);
+
+
     },
 
     getListOfObjectNumbers:(response) => {
       let list = response.artObjects;
       return tempList = list.map(function(val){ return val.objectNumber; });
+    },
+
+    addEventListenerToFigure: function() {
+      this.addEventListener('click', showRoom.appendImgToMainFigureOnClick);
     },
 
     getObjectsFromObjectNumberList: (list) => {
@@ -132,11 +164,10 @@ let Html = '';
 
 },
     //funciton that sets eventlisteners on app initiation.
-    init: () => {
+    init: function() {
       //document.getElementsByClassName('search-input')[0].addEventListener('input',showRoom.getFromSearchQuery);
       document.getElementsByClassName('search-button')[0].addEventListener('click',showRoom.getFromSearchQuery);
       //document.getElementsByClassName('search-input')[0].addEventListener('onkeypress','return showRoom.enterKey(event);');
-
     }
 };//end showRoom.
 })();
